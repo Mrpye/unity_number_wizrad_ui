3d3bee9d9550e04dfdb1471118c1e638cbbbc46e
Andrew Pye <andrewp@metsi.co.uk>
2020-01-16 15:20:30 +0000

    First Import

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour {
+    [SerializeField] private int max;
+    [SerializeField] private int min;
+    [SerializeField] TextMeshProUGUI guessText;
+    private int guess;
+
+    // Use this for initialization
+    private void Start() {
+        StartGame();
+    }
+
+    private void StartGame() {     
+        NextGuess();
+    }
+
+    public void OnPressHigher() {
+        min = guess+1;
+        NextGuess();
+    }
+
+    public void OnPressLower() {
+        max = guess-1;
+        NextGuess();
+    }
+
+    private void NextGuess() {
+        guess = Random.Range(min,max+1);
+        guessText.text = guess.ToString();
+    }
+}
